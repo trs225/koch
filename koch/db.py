30e8aea42d5719d5c7e05136200f60b85275ea4d
Trevor Samson <tsamson@tsamson-macbookpro2.roam.corp.google.com>
2018-09-18 00:17:29 -0400

    Use leveldb.

---
+++ koch/db.py
@@ -0,0 +1,103 @@
+"""Defines classes handling system i/o."""
+from __future__ import absolute_import
+
+import csv
+import plyvel
+
+from absl import logging
+
+
+class Manager(object):
+
+  defaults = {}
+
+  def __init__(self, ctor, *args, **kwargs):
+    self.ctor = ctor
+    self.args = args
+    self.kwargs = dict(self.defaults, **kwargs)
+    self.db = None
+
+  def __enter__(self):
+    self.db = self.ctor(*self.args, **self.kwargs)
+    return self
+
+  def __exit__(self, *args):
+    self.db.close()
+    self.db = None
+
+  def check(self):
+    if not self.db:
+      raise RuntimeError("Database is closed.")
+
+
+class CsvReader(Manager):
+  
+  def __init__(self, path, col=None):
+    super(CsvReader, self).__init__(open, path, 'r')
+    self.col = col
+
+  def __iter__(self):
+    self.check()
+    reader = csv.DictReader(self.db)
+    if self.col and self.col not in reader.fieldnames:
+      raise ValueError("Expected column %s in %s", self.col, self.path)
+    for row in reader:
+      yield row[self.col] if self.col else row
+
+
+class Reader(Manager):
+  
+  def __init__(self, path, **kwargs):
+    super(Reader, self).__init__(plyvel.DB, path, **kwargs)
+
+  def __iter__(self):
+    self.check()
+    with self.db.iterator() as it:
+      for key, value in it:
+        yield key, value
+
+
+class Writer(Manager):
+
+  defaults = {
+    'create_if_missing': True,
+    'error_if_exists': False,
+  }
+
+  batch_size = 100
+
+  def __init__(self, path, transaction=True, **kwargs):
+    super(Writer, self).__init__(plyvel.DB, path, **kwargs)
+    self.transaction = transaction
+    self.batch = None
+    self.n = None
+
+  def __enter__(self):
+    super(Writer, self).__enter__()
+    self._start_batch()
+    return self
+
+  def __exit__(self, *args):
+    self._end_batch(*args)
+    super(Writer, self).__exit__(*args)
+
+  def _start_batch(self):
+    self.batch = self.db.write_batch(transaction=self.transaction)
+    self.batch.__enter__()
+    self.n = 0
+
+  def _end_batch(self, *args):
+    self.batch.__exit__(*args)
+    self.batch = None
+    self.n = None
+
+  def _write_batch(self):
+    self._end_batch(None, None, None)
+    self._start_batch()
+
+  def write(self, key, value):
+    self.check()
+    self.n += 1
+    self.batch.put(key, value)
+    if self.n > self.batch_size:
+      self._write_batch()
--- koch/fetch.py
+++ koch/fetch.py
@@ -9,6 +9,7 @@ from absl import app
 from absl import flags
 from absl import logging
 
+from koch import db
 from koch.proto import document_pb2
 
 FLAGS = flags.FLAGS
@@ -18,76 +19,28 @@ flags.DEFINE_string("output", None, "Output path to write fetched html to.")
 flags.DEFINE_string(
     "column", "URL for Content", "Name of url column in csv file.")
 
+
+HEADERS = {
+  "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
+}
+
+
 def fetch(url):
   try:
-    with contextlib.closing(urllib2.urlopen(url)) as conn:
+    r = urllib2.Request(url, headers=HEADERS)
+    with contextlib.closing(urllib2.urlopen(r)) as conn:
       logging.info("Fetching %s", url)
       return conn.read()
-  except urllib2.URLError as e:
+  except Exception as e:
     logging.warning("Failed to open url %s: %s", url, str(e))
 
 
-class FileManager(object):
-
-  def __init__(self, path, mode):
-    self.path = path
-    self.mode = mode
-    self.file = None
-
-  def __enter__(self):
-    self.file = open(self.path, self.mode)
-    return self
-
-  def __exit__(self, *args):
-    self.file.close()
-    self.file = None
-
-  def check(self):
-    if not self.file:
-      raise ValueError("Expected file to be open: %s", self.path)
-
-
-class Reader(FileManager):
-
-  def __init__(self, path, col):
-    super(Reader, self).__init__(path, "r")
-    self.col = col
-
-  def __iter__(self):
-    self.check()
-    reader = csv.DictReader(self.file)
-    if self.col not in reader.fieldnames:
-      raise ValueError("Expected column %s in %s", self.col, self.path)
-    for row in reader:
-      url = row[self.col]
-      yield url
-
-
-def _format(*args):
-  return ",".join(str(i) for i in args) + "\n"
-
-
-class Writer(FileManager):
-
-  def __init__(self, path):
-    super(Writer, self).__init__(path, "w")
-
-  def write(self, *content):
-    self.check()
-    self.file.write(
-        _format(*content))
-
-
 def main(argv):
-  with Reader(FLAGS.input, FLAGS.column) as r:
-    with Writer(FLAGS.output) as w:
-      i = 0
+  with db.CsvReader(FLAGS.input, FLAGS.column) as r:
+    with db.Writer(FLAGS.output) as w:
       for url in r:
-        if i > 9:
-          break
         html = fetch(url) or ''
         w.write(url, html.encode('string-escape'))        
-        i += 1
 
 
 if __name__ == "__main__":
