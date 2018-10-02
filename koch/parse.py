ea02b8d2b9b94da46fc89fa35eab71dc5e19a4ec
Trevor Samson <tsamson@tsamson-macbookpro2.roam.corp.google.com>
2018-10-01 20:13:06 -0400

    Parse based on max subtree.

--- koch/parse.py
+++ koch/parse.py
@@ -1,6 +1,10 @@
-"""Fetches raw html content from urls."""
+"""Fetches raw html content from urls.
+
+TODO: Allow consecutive children instead of full subtree.
+"""
 from __future__ import absolute_import
 
+import collections
 import html5lib
 
 from absl import app
@@ -11,41 +15,78 @@ from koch import db
 from koch.proto import document_pb2
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string("input", None, "Input path to csv of urls to fetch.")
-flags.DEFINE_string("output", None, "Output path to write fetched html to.")
+flags.DEFINE_string("input", None, "Input raw html db to process.")
+flags.DEFINE_string("output", None, "Output path to write parsed html to.")
 
 
-def parse(raw):
-  return html5lib.parse(raw, treebuilder="etree", namespaceHTMLElements=False)
+def is_valid(tree):
+  if callable(tree.tag):
+    return False
+  if tree.tag in ('script', 'style'):
+    return False
+  return True
 
 
-_ALLOWED = set((
-    'html', 'body', 'section', 'article', 'div', 'p',
-    'h1', 'h2', 'h3', 'a', 'i', 'em', 'b'))
-
-def walk(tree, level=0, out=[]):
-  if tree.tag not in _ALLOWED:
-    return
-  if len(out) < level + 1:
-    out.append(0)
+def text_len(unicode):
+  if not unicode or unicode.encode('utf-8').decode('string_escape').isspace():
+    return 0
   else:
-    out[level] += 1
-  if tree.text and not tree.text.encode('utf-8').decode('unicode_escape').isspace():
-    print tree.tag, out[:level+1], tree.text
+    return len(unicode)
+
+
+def measure(tree):
+  if not is_valid(tree):
+    return 0  # TODO: return None instead
+  pos = text_len(tree.text) + text_len(tree.tail)
+  neg = 2 * len(tree.tag) + len(str(tree.attrib or ''))
+  return pos - neg
+
+
+class Best(object):
+  
+  def __init__(self, tree, val):
+    self.tree = tree
+    self.val = val
+
+
+def find_best(tree, best):
+  children = 0
   for child in tree:
-    walk(child, level=level+1, out=out)
-  if tree.tail and not tree.tail.encode('utf-8').decode('unicode_escape').isspace():
-    print tree.tag, out[:level+1], tree.tail
+    children += find_best(child, best)
+  val = measure(tree) + children
+  if val > best.val:
+    best.tree = tree
+    best.val = val
+  return val
+
+
+def walk(tree):
+  if not is_valid(tree):
+    return
+  if tree.text:
+    print tree.text.encode('utf-8').decode('string_escape')
+  for child in tree:
+    walk(child)
+  if tree.tail:
+    print tree.tail.encode('utf-8').decode('string_escape')
+  
+
+def parse(tree):
+  pass
 
 
 def main(argv):
   with db.Reader(FLAGS.input) as r:
     for url, raw in r:
-      print
+      tree = html5lib.parse(raw, treebuilder="etree", namespaceHTMLElements=False)
+      best = Best(None, None)
+      find_best(tree, best)
+
       print url
       print
-      tree = parse(raw) or ''
-      walk(tree)
+      walk(best.tree)
+      print
+      print
 
 
 if __name__ == "__main__":
