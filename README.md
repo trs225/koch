2cff01f0ed2c53c9e4e4aa4f65ae3a023f17ead3
Trevor Samson <tsamson@tsamson-macbookpro2.roam.corp.google.com>
2018-12-30 18:33:05 -0500

    Add nltk tokenization.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 ## Python
 
 * [html5lib](https://html5lib.readthedocs.io/en/latest/)
+* [nltk](https://www.nltk.org/book/)
 * [plyvel](https://plyvel.readthedocs.io/en/latest/)
 * [urllib2](https://docs.python.org/2/library/urllib2.html)
 * [xml.etree.ElementTree](https://docs.python.org/2/library/xml.etree.elementtree.html)
--- koch/parse.py
+++ koch/parse.py
@@ -1,7 +1,12 @@
-"""Parses document text from article html."""
+"""Parses document text from article html.
+
+TODO:
+ - filter on blob length/position
+ - sentence segmentation
+"""
 from __future__ import absolute_import
 
-import html5lib
+import nltk
 import re2
 
 from absl import app
@@ -15,33 +20,57 @@ from koch.proto import document_pb2
 FLAGS = flags.FLAGS
 flags.DEFINE_string("parse_output", None, "Output path to write parsed html to.")
 
+flags.DEFINE_boolean("parse_debug", False, "Whether to use the debug writer.")
 
-def build_doc_helper(node, doc):
+
+def add_blob(doc, text, pos):
+  blob = doc.blobs.add()
+  blob.text = text
+  blob.position.extend(pos)
+
+
+def build_doc_helper(node, doc, pos):
   if node.text:
-    doc.blobs.add().text = node.text
-  for child in node.children:
-    build_doc_helper(child, doc)
+    add_blob(doc, node.text, pos)
+  for i, child in enumerate(node.children):
+    build_doc_helper(child, doc, pos + [i])
   if node.tail:
-    doc.blobs.add().text = node.tail
+    add_blob(doc, node.tail, pos[:-1])
 
 
 def build_doc(nodes):
   doc = document_pb2.Document()
-  for node in nodes.elements:
-    build_doc_helper(node, doc)
+  for i, node in enumerate(nodes.elements):
+    build_doc_helper(node, doc, [i])
   return doc
 
 
 class ParsingPipeline(pipeline.Pipeline):
+
+  def __init__(self, reader, writer=None):
+    super(ParsingPipeline, self).__init__(reader, writer)
+    self.stopwords = set(nltk.corpus.stopwords.words("english"))
+    self.wordnet = nltk.WordNetLemmatizer()
   
   def pipe(self, key, value):
-    return key, build_doc(value)
+    doc = build_doc(value)
+    for blob in doc.blobs:
+      tokens = (t.lower() for t in nltk.word_tokenize(blob.text))
+      stopped = (t for t in tokens if t not in self.stopwords)
+      normalized = (re2.sub(r"\W+", "", t) for t in stopped)
+      lemmatized = (self.wordnet.lemmatize(t) for t in normalized if t)
+      blob.words.extend(lemmatized)
+
+    return key, doc
   
 
 def main(argv):
   reader = db.ProtoDbReader(document_pb2.RawHtml, FLAGS.extract_input)
   extractor = extract.ExtractionPipeline(reader)
   writer = db.ProtoDbWriter(document_pb2.Document, FLAGS.parse_output)
+
+  if FLAGS.parse_debug:
+    writer = db.DebugWriter()
 
   ParsingPipeline(extractor, writer).run()
  
